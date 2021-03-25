6985311c9808351dd4b30192a8978c27e352f0e2
Ievgenii Makhonko <yobaua@gmail.com>
2021-03-25 16:08:41 +0800

    Commit

---
+++ DindTheAngleV2/DindTheAngleV2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace FindTheAngleV2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number to find out the angle: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+            int degree = GetDegree(x);
+            Console.WriteLine("Degree is " + degree);
+        }
+
+        private static int GetDegree(int x)
+        {
+            int y = x % 12;
+            int degree = NormalizedNumber(y) * 30;
+            return degree;
+        }
+
+        public static int NormalizedNumber(int number)
+        {
+            if (number > 6)
+            {
+                return 12 - number;
+            }
+            return number;
+        }
+    }
+}
